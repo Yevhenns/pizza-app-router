1f3d123a06989a9549884a6728363a75a93eecdf
Yevhenns <gifestum@gmail.com>
2024-07-06 13:57:52 +0300

    hot fix

--- src/helpers/filterByCategory.ts
+++ src/helpers/filterByCategory.ts
@@ -1,3 +1,3 @@
-export const filterByCategory = (data: TProductsArr, category: string) => {
-  return data.filter((item: TProduct) => item.category === category);
+export const filterByCategory = (data: Product[], category: string) => {
+  return data.filter(item => item.category === category);
 };

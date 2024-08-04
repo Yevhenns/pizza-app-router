3c144b2823b32e9911e74740abaf74ce4bae0e44
Yevhenns <gifestum@gmail.com>
2024-08-04 13:31:34 +0300

    removed server

--- next.config.mjs
+++ next.config.mjs
@@ -14,12 +14,6 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'objectstorage.eu-frankfurt-1.oraclecloud.com',
-        port: '',
-        pathname: '/n/fr0trzcie6nl/b/pizza/o/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/dyka4vajb/image/upload/**',

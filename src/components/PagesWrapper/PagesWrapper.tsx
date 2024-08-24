53913efe6bec2f56324afca51c59703efc6aa372
yevhenns <gifestum@gmail.com>
2024-08-24 16:19:05 +0300

    deleted hook

--- src/components/PagesWrapper/PagesWrapper.tsx
+++ src/components/PagesWrapper/PagesWrapper.tsx
@@ -1,32 +1,35 @@
 import { PropsWithChildren, useEffect } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { useFetchProducts } from '@/hooks/useFetchProducts';
 import { checkCart } from '@/redux/cart/cartSlice';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
-import { getIsLoading, getProductsAll } from '@/redux/products/productsSlice';
+import { getError, getIsLoading, getProductsAll } from '@/redux/products/productsSlice';
 
 import { Error500 } from '@/components/Error500';
 import { Container } from '@/components/common/Container';
 import { Section } from '@/components/common/Section';
 
 import { LoaderModal } from '../common/LoaderModal';
+import { getProducts } from '@/redux/products/productsOperations';
 
-interface PagesWrapperProps extends PropsWithChildren {}
+type PagesWrapperProps = PropsWithChildren;
 
 export function PagesWrapper({ children }: PagesWrapperProps) {
-  const error = useFetchProducts();
-
   const productsAll = useAppSelector(getProductsAll);
   const isLoading = useAppSelector(getIsLoading);
+  const error = useAppSelector(getError);
 
   const dispatch = useAppDispatch();
 
   useEffect(() => {
+    if (productsAll.length === 0 && !error) {
+      dispatch(getProducts());
+      return;
+    }
     if (productsAll.length > 0) {
       dispatch(checkCart(productsAll));
     }
-  }, [dispatch, productsAll]);
+  }, [dispatch, error, productsAll]);
 
   return (
     <Section>

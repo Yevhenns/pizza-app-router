3b234c78a3f93e304bdd8e0cd1191101ca4fb15e
Yevhenns <gifestum@gmail.com>
2024-08-28 09:40:30 +0300

    hot fix

--- src/components/PagesWrapper/PagesWrapper.tsx
+++ src/components/PagesWrapper/PagesWrapper.tsx
@@ -3,14 +3,18 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import { checkCart } from '@/redux/cart/cartSlice';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
-import { getError, getIsLoading, getProductsAll } from '@/redux/products/productsSlice';
+import { getProducts } from '@/redux/products/productsOperations';
+import {
+  getError,
+  getIsLoading,
+  getProductsAll,
+} from '@/redux/products/productsSlice';
 
 import { Error500 } from '@/components/Error500';
 import { Container } from '@/components/common/Container';
 import { Section } from '@/components/common/Section';
 
 import { LoaderModal } from '../common/LoaderModal';
-import { getProducts } from '@/redux/products/productsOperations';
 
 type PagesWrapperProps = PropsWithChildren;
 
@@ -22,14 +26,14 @@ export function PagesWrapper({ children }: PagesWrapperProps) {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    if (productsAll.length === 0 && !error) {
-      dispatch(getProducts());
-      return;
-    }
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  useEffect(() => {
     if (productsAll.length > 0) {
       dispatch(checkCart(productsAll));
     }
-  }, [dispatch, error, productsAll]);
+  }, [dispatch, productsAll]);
 
   return (
     <Section>

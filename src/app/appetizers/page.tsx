a66077541b23a906d8ff0a881d43b1a86bd1f9a3
Yevhenns <gifestum@gmail.com>
2024-10-06 20:19:41 +0300

    revalidate

--- src/app/appetizers/page.tsx
+++ src/app/appetizers/page.tsx
@@ -1,5 +1,7 @@
 import { ProductsList } from '@/components/ProductsList';
 
+export const revalidate = 0;
+
 export default function Appetizers() {
   return <ProductsList category="appetizers" />;
 }

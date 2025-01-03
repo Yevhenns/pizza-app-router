eeb01990e72d5be43857abf54698b50338486029
Yevhenns <gifestum@gmail.com>
2025-01-03 12:55:31 +0200

    redirect rule

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,22 @@
+'use client';
+
+import { useLayoutEffect } from 'react';
+
+import { redirect } from 'next/navigation';
+
+import { getUserInfo } from '@/redux/auth/authSlice';
+import { useAppSelector } from '@/redux/hooks';
+
 export default function Admin() {
+  const ADMIN_ID = process.env.ADMIN_ID;
+
+  const userInfo = useAppSelector(getUserInfo);
+
+  useLayoutEffect(() => {
+    if (userInfo?.sub !== ADMIN_ID) {
+      redirect('/');
+    }
+  }, [ADMIN_ID, userInfo?.sub]);
+
   return <div>Адмінка</div>;
 }

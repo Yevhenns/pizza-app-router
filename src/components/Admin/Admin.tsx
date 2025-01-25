20493fdab76fa4ac24cac57a6fb4169a324fcb8f
Yevhenns <gifestum@gmail.com>
2025-01-25 12:49:03 +0200

    admin refactoring

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -1,51 +1,33 @@
 'use client';
 
-import { useEffect, useLayoutEffect, useState } from 'react';
+import { useLayoutEffect } from 'react';
 
 import { redirect } from 'next/navigation';
 
 import { useWindowWidth } from '@/hooks/useWindowWidth';
 import { getUserInfo } from '@/store/auth/authSlice';
 import { useAppSelector } from '@/store/hooks';
-import { getProducts } from '@/store/products/productsOperations';
+import { getProductsAll } from '@/store/products/productsSlice';
 
 import { ProductsTable } from './ProductsTable/ProductsTable';
 
 export default function Admin() {
-  const [products, setProducts] = useState<Product[] | null>(null);
-  const [loading, setLoading] = useState(true);
-
   const ADMIN_ID = process.env.ADMIN_ID;
 
   const userInfo = useAppSelector(getUserInfo);
+  const products = useAppSelector(getProductsAll);
 
   const width = useWindowWidth();
 
-  console.log(products);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const data = await getProducts();
-        setProducts(data);
-      } catch (error) {
-        console.error('Помилка:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchProducts();
-  }, []);
+  const minimalScreenWidth = 768;
 
   useLayoutEffect(() => {
-    if (userInfo?.sub !== ADMIN_ID || width <= 768) {
+    if (userInfo?.sub !== ADMIN_ID || width <= minimalScreenWidth) {
       redirect('/');
     }
   }, [ADMIN_ID, userInfo?.sub, width]);
 
   return (
-    <div>
-      <ProductsTable products={products} />
-    </div>
+    <div>{products.length > 0 && <ProductsTable products={products} />}</div>
   );
 }

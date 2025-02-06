f76f885bf67e17c7efeb67e01ea0b1d423b4d029
Yevhenns <gifestum@gmail.com>
2025-02-06 08:37:41 +0200

    added url

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -5,7 +5,7 @@ export default function manifest(): MetadataRoute.Manifest {
     name: 'Next.js PWA',
     short_name: 'NextPWA',
     description: 'A Progressive Web App built with Next.js',
-    start_url: '/',
+    start_url: 'https://nostrra-pizzza.vercel.app/',
     display: 'standalone',
     background_color: '#ffffff',
     theme_color: '#000000',

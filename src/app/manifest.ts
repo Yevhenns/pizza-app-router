5ce7e6665c8b50103b6852293503322a3df182fb
Yevhenns <gifestum@gmail.com>
2025-02-06 08:48:21 +0200

    added id

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -6,6 +6,7 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name: 'NextPWA',
     description: 'A Progressive Web App built with Next.js',
     start_url: '/',
+    id: '/',
     display: 'standalone',
     background_color: '#ffffff',
     theme_color: '#000000',

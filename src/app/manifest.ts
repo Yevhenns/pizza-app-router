2186072b261c156e342421ec2f10761d478d47d7
Yevhenns <gifestum@gmail.com>
2025-02-06 08:06:12 +0200

    fixed path

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: './favicon.ico',
+        src: '/favicon.ico',
         sizes: '192x192',
         type: 'image/ico',
       },
       {
-        src: './favicon.icog',
+        src: '/favicon.ico',
         sizes: '512x512',
         type: 'image/ico',
       },

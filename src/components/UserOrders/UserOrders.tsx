7d335e4cfbb96a4f9bf11389d2085e2a9951537b
Yevhenns <gifestum@gmail.com>
2025-02-09 14:24:54 +0200

    refactoring

--- src/components/UserOrders/UserOrders.tsx
+++ src/components/UserOrders/UserOrders.tsx
@@ -35,29 +35,31 @@ export function UserOrders({ logoutHandler, userInfo }: UserOrdersProps) {
       {userOrders.length === 0 ? (
         <span>Список замовлень порожній</span>
       ) : (
-        <div className={css.orderWrapper}>
+        <>
           <h3>Список замовлень</h3>
-          {userOrders.map(item => (
-            <ul key={item._id} className={css.order}>
-              <span>{formattedDate(item.createdAt)}</span>
-              {item.order.map(item => (
-                <li key={item._id}>
-                  <div className={css.titleQuantity}>
-                    <span>{item.title}</span>
-                    <span>{item.quantity} шт.</span>
-                  </div>
-                  <div className={css.optionsWrapper}>
-                    {item.optionsTitles.length > 0 &&
-                      item.optionsTitles.map(opt => (
-                        <span key={opt}>{opt}</span>
-                      ))}
-                  </div>
-                </li>
-              ))}
-              <span>Загальна сума: {item.orderSum} грн.</span>
-            </ul>
-          ))}
-        </div>
+          <div className={css.orderWrapper}>
+            {userOrders.map(item => (
+              <ul key={item._id} className={css.order}>
+                <span>{formattedDate(item.createdAt)}</span>
+                {item.order.map(item => (
+                  <li key={item._id}>
+                    <div className={css.titleQuantity}>
+                      <span>{item.title}</span>
+                      <span>{item.quantity} шт.</span>
+                    </div>
+                    <div className={css.optionsWrapper}>
+                      {item.optionsTitles.length > 0 &&
+                        item.optionsTitles.map(opt => (
+                          <span key={opt}>+ {opt}</span>
+                        ))}
+                    </div>
+                  </li>
+                ))}
+                <span>Загальна сума: {item.orderSum} грн.</span>
+              </ul>
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
--- src/models/UserOrder.ts
+++ src/models/UserOrder.ts
@@ -4,11 +4,11 @@ export interface UserOrder extends mongoose.Document, SummaryOrder {}
 
 const customerInfoSchema = new mongoose.Schema<CustomerInfo>(
   {
-    address: { type: String, required: false },
-    comment: { type: String, required: false },
-    name: { type: String, required: true },
-    number: { type: String, required: true },
-    userId: { type: String, required: false },
+    address: { type: String, maxlength: 200, required: false },
+    comment: { type: String, maxlength: 200, required: false },
+    name: { type: String, maxlength: 200, required: true },
+    number: { type: String, maxlength: 200, required: true },
+    userId: { type: String, maxlength: 200, required: false },
   },
   {
     versionKey: false,
@@ -17,8 +17,8 @@ const customerInfoSchema = new mongoose.Schema<CustomerInfo>(
 
 const orderedSchema = new mongoose.Schema<Ordered>(
   {
-    title: { type: String, required: true },
-    quantity: { type: Number, required: true },
+    title: { type: String, maxlength: 200, required: true },
+    quantity: { type: Number, min: 1, max: 200, required: true },
     optionsTitles: { type: [String], default: [], required: false },
   },
   {
@@ -30,7 +30,7 @@ const userOrderSchema = new mongoose.Schema<UserOrder>(
   {
     customerInfo: { type: customerInfoSchema, required: true },
     order: { type: [orderedSchema], required: true },
-    orderSum: { type: Number, required: true },
+    orderSum: { type: Number, min: 1, max: 10000, required: true },
   },
   {
     versionKey: false,

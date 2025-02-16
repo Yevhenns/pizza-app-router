775d96c305470b22c964bc51f29cfb47259bd9ff
Yevhenns <gifestum@gmail.com>
2025-02-16 15:12:45 +0200

    testing headers

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 import withPWA from 'next-pwa';
 
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [{ key: 'Cross-Origin-Opener-Policy', value: 'same-origin' }],
+      },
+    ];
+  },
   env: {
     GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
     WEATHER_API_KEY: process.env.WEATHER_API_KEY,

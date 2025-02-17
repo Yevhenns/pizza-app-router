61b6c21822ee7e4c8b87cf2facf5eae8d256941e
Yevhenns <gifestum@gmail.com>
2025-02-17 14:04:46 +0200

    testing swagger-jsdoc

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -1,27 +1,27 @@
-import { createSwaggerSpec } from 'next-swagger-doc';
+import swaggerJSDoc from 'swagger-jsdoc';
 
-import Product from '@/models/Product';
-
-export const getApiDocs = async () => {
-  const spec = createSwaggerSpec({
-    apiFolder: 'src/app/api/v1',
-    definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'Next Swagger API Example',
-        version: '1.0',
-      },
-      components: {
-        securitySchemes: {
-          BearerAuth: {
-            type: 'http',
-            scheme: 'bearer',
-            bearerFormat: 'JWT',
-          },
-        },
-      },
-      security: [],
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: 'Документація для API',
     },
-  });
-  return spec;
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Local server',
+      },
+      {
+        url: 'https://nostrra-pizzza.vercel.app',
+        description: 'Production server',
+      },
+    ],
+  },
+  apis: ['./src/app/api/v1/**/*.ts'],
 };
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+export default swaggerSpec;

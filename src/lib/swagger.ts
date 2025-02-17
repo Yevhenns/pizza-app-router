88b4307a90822d2571a5123aae0763bdb43f8d5f
Yevhenns <gifestum@gmail.com>
2025-02-17 14:28:10 +0200

    testing swagger

---
+++ src/lib/swagger.ts
@@ -0,0 +1,25 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Nostra Pizza',
+      version: '1.0.0',
+      description: 'API Documentation',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Local server',
+      },
+      {
+        url: 'https://nostrra-pizzza.vercel.app',
+        description: 'Production server',
+      },
+    ],
+  },
+  apis: ['./src/app/api/v1/**/*.ts'],
+};
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+export default swaggerSpec;
